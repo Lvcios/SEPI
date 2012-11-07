e3887083937fbcb344ec545d28ec96a15f709ef0
Lucio Flores <lvcios.informatico@gmail.com>
2012-11-06 23:09:03 -0600

    Primera versión funcional

--- docgeneratorcp/admin.py
+++
@@ -1,7 +0,0 @@
-from docgenerator.models import Oficio,Destino,OfiDest,OfiCC
-from django.contrib import admin
-
-admin.site.register(Oficio)
-admin.site.register(Destino)
-admin.site.register(OfiDest)
-admin.site.register(OfiCC)
