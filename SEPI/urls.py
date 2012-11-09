c63a589dba4ae8c7831290c03a46849e2c260719
Lucio Flores <lvcios.informatico@gmail.com>
2012-11-08 21:41:05 -0600

    media path

--- SEPI/urls.py
+++ SEPI/urls.py
@@ -23,5 +23,6 @@ urlpatterns = patterns('',
 
 if not settings.DEBUG:
     urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+        url(r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.MEDIA_ROOT,},),
     )
